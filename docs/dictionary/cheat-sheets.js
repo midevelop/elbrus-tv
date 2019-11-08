a34f47933e68f8a766176a8c9a82e768f7885f07
Mikhail Ivanov <55839915+midevelop@users.noreply.github.com>
2019-11-08 14:32:50 +0300

    feat(docs): add page on git flow

--- docs/dictionary/cheat-sheets.js
+++ docs/dictionary/cheat-sheets.js
@@ -1,15 +1,23 @@
 const linksCheatScheets = [
   {
-    src: "https://fb.ru/misc/i/gallery/48362/1780757.jpg",
-    title: "Git 1"
+    src: 'https://fb.ru/misc/i/gallery/48362/1780757.jpg',
+    title: 'Git 1'
   },
   {
-    src: "https://fb.ru/misc/i/gallery/48362/1780756.jpg",
-    title: "Git 2"
+    src: 'https://fb.ru/misc/i/gallery/48362/1780756.jpg',
+    title: 'Git 2'
   },
   {
     src:
-      "https://jrebel.com/wp-content/uploads/2016/02/Git-Cheat-Sheet-pdf-v2.png",
-    title: "Git 3"
+      'https://jrebel.com/wp-content/uploads/2016/02/Git-Cheat-Sheet-pdf-v2.png',
+    title: 'Git 3'
+  },
+  {
+    src: 'https://bitworks.software/assets/img/2018-12-11/DevelopAndMaster.png',
+    title: 'Git Flow 1'
+  },
+  {
+    src: 'https://miro.medium.com/max/1029/1*JuVr5mROemkMYdIAgmStLg.png',
+    title: 'Git Flow 2'
   }
 ];
